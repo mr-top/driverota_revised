78af96b121773fa51f039a3276e253918f11ff45
Shuka <tserenlkhagvaganbold@gmail.com>
2025-08-25 12:52:02 +0100

    signup height fix

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,6 @@
 function Signup() {
   return (
-    <div className="flex flex-col justify-center items-center space-y-5 max-w-100 w-screen h-140 bg-base-200 border-[0.5px] border-base-content rounded-md">
+    <div className="flex flex-col justify-start items-center space-y-5 py-15 max-w-100 w-screen min-h-140 bg-base-200 border-[0.5px] border-base-content rounded-md">
       <div>
         <p className="text-3xl">Sign Up</p>
       </div>
