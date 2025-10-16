fe01a04a5513445e3cae3f46ba58b6ae01425153
Admin <tserenlkhagvaganbold@gmail.com>
2025-10-17 00:43:24 +0100

    profile route elobarated

--- src/components/redirects/ProfileRoute.jsx
+++ src/components/redirects/ProfileRoute.jsx
@@ -3,14 +3,14 @@ import { Outlet } from "react-router-dom";
 
 import { getProfile, getSession } from "../../awConfig";
 
-function ProfileRoute({localProfile, logout}) {
+function ProfileRoute({ localProfile, logout }) {
   const [fetchedProfile, setFetchedProfile] = useState({ fetched: false });
 
   useEffect(() => {
     if (localProfile.logged) {
       fetchProfile(localProfile.id);
     } else {
-      setFetchedProfile({fetched: true, logged: false});
+      setFetchedProfile({ fetched: true, logged: false });
     }
   }, []);
 
@@ -21,10 +21,10 @@ function ProfileRoute({localProfile, logout}) {
     const sessionResult = await getSession();
 
     if (!sessionResult.success || sessionResult.session?.userId !== result.profile?.$id) {
-      // if there isn't session, log out
+      // if there isn't session or session user id and fetched profile id does not match, log out
       await logout();
     }
-    
+
     // user must exist and there must be a cloud logged session to be logged. Session user id and fetched profile id must also match
     const logged = result.success && sessionResult.success && (result.profile.$id === sessionResult.session.userId);
 
@@ -33,7 +33,7 @@ function ProfileRoute({localProfile, logout}) {
 
   return (
     fetchedProfile.fetched ?
-      <Outlet context={{ fetchedProfile }}/> :
+      <Outlet context={{ fetchedProfile }} /> :
       <span className="loading loading-spinner loading-md"></span>
   )
 }
